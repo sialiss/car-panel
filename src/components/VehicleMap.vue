cc9ae98ea35567a2bfaf226ee63dcdfd9a969521
Camellia <56206613+sialiss@users.noreply.github.com>
2025-10-15 20:56:26 +0300

    update for gh-pages

--- src/components/VehicleMap.vue
+++ src/components/VehicleMap.vue
@@ -6,6 +6,16 @@
 import { onMounted, watch } from 'vue'
 import type { Vehicle } from '@/types/Vehicle'
 import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+import iconUrl from 'leaflet/dist/images/marker-icon.png'
+import iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png'
+import shadowUrl from 'leaflet/dist/images/marker-shadow.png'
+// delete L.Icon.Default.prototype._getIconUrl
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+})
 
 const props = defineProps<{ vehicles: Vehicle[] }>()
 
--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,11 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
+const deployEnv = process.env.DEPLOY_ENV
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: deployEnv === 'GH_PAGES' ? '/car-panel/' : '/',
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
